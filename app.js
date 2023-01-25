6cd1334b3606ac08f04f66cfe91590adac132893
Sudam Majhi <sudammajhi@users.noreply.github.com>
2023-01-25 11:42:51 +0530

    Calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+
+let calculation = []
+let accumulativeCalculation
+
+function calculator(button){
+    const value = button.textContent
+    if(value=="Clear"){
+        calculation = []
+        screenDisplay.textContent = '.'
+    }else if (value == '='){
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    }else{calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculator(button)))
